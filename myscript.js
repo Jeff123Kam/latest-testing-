b1da2a5450a1976a193a42a5ad4c4c04ce71d3ce
Jeff123Kam <48338972+Jeff123Kam@users.noreply.github.com>
2019-09-26 16:19:03 +0930

    Add files via upload

---
+++ myscript.js
@@ -0,0 +1,63 @@
+function initMap() {
+    var location = {lat: -12.463440, lng: 130.845642};
+    var map = new google.maps.Map(document.getElementById("map"),{
+        zoom: 4,
+        center:location
+    });
+    var marker = new google.maps.Marker({
+        position:location,
+        map: map
+    })
+}
+
+let sliderImages = document.querySelectorAll(".slide"),
+    arrowLeft = document.querySelector("#arrow-left"),
+    arrowRight = document.querySelector("#arrow-right"),
+    current = 0;
+
+// Clear all images
+function reset() {
+    for (let i = 0; i < sliderImages.length; i++) {
+        sliderImages[i].style.display = "none";
+    }
+}
+
+// Init slider
+function startSlide() {
+    reset();
+    sliderImages[0].style.display = "block";
+}
+
+// Show prev
+function slideLeft() {
+    reset();
+    sliderImages[current - 1].style.display = "block";
+    current--;
+}
+
+// Show next
+function slideRight() {
+    reset();
+    sliderImages[current + 1].style.display = "block";
+    current++;
+}
+
+// Left arrow click
+arrowLeft.addEventListener("click", function() {
+    if (current === 0) {
+        current = sliderImages.length;
+    }
+    slideLeft();
+});
+
+// Right arrow click
+arrowRight.addEventListener("click", function() {
+    if (current === sliderImages.length - 1) {
+        current = -1;
+    }
+    slideRight();
+});
+
+startSlide();
+
+
